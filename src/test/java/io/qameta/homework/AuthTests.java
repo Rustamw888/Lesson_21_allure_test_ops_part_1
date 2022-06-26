8423b4df237ba0b27650573a3289e7c097f4f6a7
qaMilord <rustamw888@gmail.com>
2022-06-27 01:35:17 +0300

    first step

---
+++ src/test/java/io/qameta/homework/AuthTests.java
@@ -0,0 +1,12 @@
+package io.qameta.homework;
+
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.Test;
+
+public class AuthTests {
+
+    @Test
+    public void testGoogleAuth() {
+
+    }
+}
