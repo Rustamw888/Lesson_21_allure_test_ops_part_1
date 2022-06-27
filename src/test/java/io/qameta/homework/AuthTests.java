f03d712041188739abcd43af5ca3a5caab2d506d
qaMilord <rustamw888@gmail.com>
2022-06-27 12:34:19 +0300

    plugin added

--- src/test/java/io/qameta/homework/AuthTests.java
+++ src/test/java/io/qameta/homework/AuthTests.java
@@ -1,11 +1,40 @@
 package io.qameta.homework;
 
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 
 public class AuthTests {
 
     @Test
+    @AllureId("10710")
+    @DisplayName("Авторизация через Google")
+    @Tags({@Tag("blocker"), @Tag("web")})
+    @Story("Авторизация через сторонние системы")
+    @Owner("admin1")
+    @Feature("Авторизация")
     public void testGoogleAuth() {
-
+        step("Открываем главную страницу");
+        step("Нажимаем кнопку Авторизация");
+        step("Выбираем способ авторизации через Google");
+        step("Авторизуемся как пользователь 'user_name'", () -> {
+            step("Вводим логин 'user_login'");
+            step("Вводим пароль 'user_password'");
+            step("Нажимаем кнопку Войти");
+        });
+        step("Должны оказаться на главной странице сайта");
+        step("Профиль пользователя должен быть заполнен из Google", () -> {
+            step("Имя 'user_name'");
+            step("Login 'user_login'");
+            step("Аватарка ");
+        });
+        step("Разлогиниваемся");
     }
 }
